85a3da6a55489adaae3893c199fbf0a9450347a8
Viacheslav Tykhovlis <front.tykhovlis@gmail.com>
2022-08-26 16:38:45 +0200

    Add task-1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,45 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const blokGallery = document.querySelector(".gallery");
+blokGallery.innerHTML = createElements(galleryItems);
+blokGallery.addEventListener("click", openModal);
+
+function openModal(evt) {
+  if (!evt.target.classList.contains("gallery__image")) {
+    return;
+  }
+
+  evt.preventDefault();
+  const originalImage = basicLightbox.create(`
+    <img src="${evt.target.dataset.source}" width="1280" height="854">
+`);
+  originalImage.show();
+  window.addEventListener("keydown", closeModal);
+
+  function closeModal(evt) {
+    if (evt.code === "Escape") {
+      originalImage.close();
+      window.removeEventListener("keydown", closeModal);
+    }
+  }
+}
+
+function createElements(gallery) {
+  const imageElements = gallery
+    .map(
+      ({ preview, original, description }) =>
+        `<div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+         <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+        />
+        </a>
+    </div>`
+    )
+    .join("");
+  return imageElements;
+}
