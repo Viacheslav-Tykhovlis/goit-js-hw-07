02cdb8d70f4d1274e18b85f262b25992e3bf5e74
Viacheslav Tykhovlis <front.tykhovlis@gmail.com>
2022-08-26 22:28:26 +0200

    Add task-2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,22 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const blokGallery = document.querySelector(".gallery");
+blokGallery.innerHTML = createElements(galleryItems);
+
+function createElements(gallery) {
+  const imageElements = gallery
+    .map(
+      ({ preview, original, description }) =>
+        `<li class="gallery__item"><a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a></li>`
+    )
+    .join("");
+  return imageElements;
+}
+
+new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
